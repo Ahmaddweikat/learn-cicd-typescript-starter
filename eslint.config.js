c8d882f0af34f886e4db86d8f9d2a5c843187af2
Ahmaddweikat <a.dwekat.1999@gmail.com>
2026-03-28 16:04:14 +0300

    chore: add eslint-plugin-security

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,20 @@
+import js from "@eslint/js";
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-export default [
-  { ignores: ["dist/"] }, // ← replaces .eslintignore
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default defineConfig([
+  { ignores: ["dist/"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+]);
